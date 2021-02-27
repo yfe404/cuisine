083c6e1629a5cae679f4b1a7728ddee5cda187f1
yfe404 <yfe.github@protonmail.com>
2021-02-27 12:55:59 +0100

    remove virtualenv

---
+++ main.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.jinja2')
--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 source venv/bin/activate
 
+export FLASK_PORT=7777
 export FLASK_ENV=development
 export FLASK_APP=main.py
 
-flask run
+flask run --port $FLASK_PORT
